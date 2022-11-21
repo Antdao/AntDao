dee58c57fa0eb98a65663b5a28adf207a9e20437
Antdao <94835663+Antdao@users.noreply.github.com>
2022-11-21 10:58:29 +0800

    Update IAntFarm.sol

--- contracts/interfaces/IAntFarm.sol
+++ contracts/interfaces/IAntFarm.sol
@@ -1,1 +1,11 @@
 
+pragma solidity 0.6.12;
+
+interface IAntFarm {
+
+    function initFarm(
+        bytes calldata data
+    ) external;
+    function farmTemplate() external view returns (uint256);
+
+}
