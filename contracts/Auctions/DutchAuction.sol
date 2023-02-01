dffc5a88d6b32840fb81288324bbc73a3db87bb2
Antdao <94835663+Antdao@users.noreply.github.com>
2023-02-01 15:24:17 +0800

    Update DutchAuction.sol

--- contracts/Auctions/DutchAuction.sol
+++ contracts/Auctions/DutchAuction.sol
@@ -273,4 +273,230 @@ contract DutchAuction is IAntMarket, ANTAccessControls, BoringBatchable, SafeTra
         ) / uint256(_marketInfo.endTime.sub(_marketInfo.startTime));        
         return uint256(_marketPrice.startPrice).sub(priceDiff);
     }
+    
+    function _addCommitment(address _addr, uint256 _commitment) internal {
+        require(block.timestamp >= uint256(marketInfo.startTime) && block.timestamp <= uint256(marketInfo.endTime), "DutchAuction: outside auction hours");
+        MarketStatus storage status = marketStatus;
+        
+        uint256 newCommitment = commitments[_addr].add(_commitment);
+        if (status.usePointList) {
+            require(IPointList(pointList).hasPoints(_addr, newCommitment));
+        }
+        
+        commitments[_addr] = newCommitment;
+        status.commitmentsTotal = BoringMath.to128(uint256(status.commitmentsTotal).add(_commitment));
+        emit AddedCommitment(_addr, _commitment);
+    }
 
+
+   
+    function cancelAuction() public   nonReentrant  
+    {
+        require(hasAdminRole(msg.sender));
+        MarketStatus storage status = marketStatus;
+        require(!status.finalized, "DutchAuction: auction already finalized");
+        require( uint256(status.commitmentsTotal) == 0, "DutchAuction: auction already committed" );
+        _safeTokenPayment(auctionToken, wallet, uint256(marketInfo.totalTokens));
+        status.finalized = true;
+        
+        emit AuctionCancelled();
+    }
+
+    
+    function finalize() public   nonReentrant  
+    {
+
+        require(hasAdminRole(msg.sender) 
+                || hasSmartContractRole(msg.sender) 
+                || wallet == msg.sender
+                || finalizeTimeExpired(), "DutchAuction: sender must be an admin");
+        
+        require(marketInfo.totalTokens > 0, "Not initialized");
+
+        MarketStatus storage status = marketStatus;
+
+        require(!status.finalized, "DutchAuction: auction already finalized");
+        if (auctionSuccessful()) {
+           _safeTokenPayment(paymentCurrency, wallet, uint256(status.commitmentsTotal));
+        } else {
+            require(block.timestamp > uint256(marketInfo.endTime), "DutchAuction: auction has not finished yet"); 
+            _safeTokenPayment(auctionToken, wallet, uint256(marketInfo.totalTokens));
+        }
+        status.finalized = true;
+        emit AuctionFinalized();
+    }
+
+
+    function withdrawTokens() public  {
+        withdrawTokens(msg.sender);
+    }
+
+   function withdrawTokens(address payable beneficiary) public   nonReentrant  {
+        if (auctionSuccessful()) {
+            require(marketStatus.finalized, "DutchAuction: not finalized");
+            uint256 tokensToClaim = tokensClaimable(beneficiary);
+            require(tokensToClaim > 0, "DutchAuction: No tokens to claim"); 
+            claimed[beneficiary] = claimed[beneficiary].add(tokensToClaim);
+            _safeTokenPayment(auctionToken, beneficiary, tokensToClaim);
+        } else {
+            require(block.timestamp > uint256(marketInfo.endTime), "DutchAuction: auction has not finished yet");
+            uint256 fundsCommitted = commitments[beneficiary];
+            commitments[beneficiary] = 0; 
+            _safeTokenPayment(paymentCurrency, beneficiary, fundsCommitted);
+        }
+    }
+
+
+    function setDocument(string calldata _name, string calldata _data) external {
+        require(hasAdminRole(msg.sender) );
+        _setDocument( _name, _data);
+    }
+
+    function setDocuments(string[] calldata _name, string[] calldata _data) external {
+        require(hasAdminRole(msg.sender) );
+        uint256 numDocs = _name.length;
+        for (uint256 i = 0; i < numDocs; i++) {
+            _setDocument( _name[i], _data[i]);
+        }
+    }
+
+    function removeDocument(string calldata _name) external {
+        require(hasAdminRole(msg.sender));
+        _removeDocument(_name);
+    }
+
+
+
+    function setList(address _list) external {
+        require(hasAdminRole(msg.sender));
+        _setList(_list);
+    }
+
+    function enableList(bool _status) external {
+        require(hasAdminRole(msg.sender));
+        marketStatus.usePointList = _status;
+
+        emit AuctionPointListUpdated(pointList, marketStatus.usePointList);
+    }
+
+    function _setList(address _pointList) private {
+        if (_pointList != address(0)) {
+            pointList = _pointList;
+            marketStatus.usePointList = true;
+        }
+
+        emit AuctionPointListUpdated(pointList, marketStatus.usePointList);
+    }
+
+    function setAuctionTime(uint256 _startTime, uint256 _endTime) external {
+        require(hasAdminRole(msg.sender));
+        require(_startTime < 10000000000, "DutchAuction: enter an unix timestamp in seconds, not miliseconds");
+        require(_endTime < 10000000000, "DutchAuction: enter an unix timestamp in seconds, not miliseconds");
+        require(_startTime >= block.timestamp, "DutchAuction: start time is before current time");
+        require(_endTime > _startTime, "DutchAuction: end time must be older than start time");
+        require(marketStatus.commitmentsTotal == 0, "DutchAuction: auction cannot have already started");
+
+        marketInfo.startTime = BoringMath.to64(_startTime);
+        marketInfo.endTime = BoringMath.to64(_endTime);
+        
+        emit AuctionTimeUpdated(_startTime,_endTime);
+    }
+
+   
+    function setAuctionPrice(uint256 _startPrice, uint256 _minimumPrice) external {
+        require(hasAdminRole(msg.sender));
+        require(_startPrice > _minimumPrice, "DutchAuction: start price must be higher than minimum price");
+        require(_minimumPrice > 0, "DutchAuction: minimum price must be greater than 0"); 
+        require(marketStatus.commitmentsTotal == 0, "DutchAuction: auction cannot have already started");
+
+        marketPrice.startPrice = BoringMath.to128(_startPrice);
+        marketPrice.minimumPrice = BoringMath.to128(_minimumPrice);
+
+        emit AuctionPriceUpdated(_startPrice,_minimumPrice);
+    }
+
+   
+    function setAuctionWallet(address payable _wallet) external {
+        require(hasAdminRole(msg.sender));
+        require(_wallet != address(0), "DutchAuction: wallet is the zero address");
+
+        wallet = _wallet;
+
+        emit AuctionWalletUpdated(_wallet);
+    }
+
+
+
+    function init(bytes calldata _data) external override payable {
+
+    }
+
+    function initMarket(
+        bytes calldata _data
+    ) public override {
+        (
+        address _funder,
+        address _token,
+        uint256 _totalTokens,
+        uint256 _startTime,
+        uint256 _endTime,
+        address _paymentCurrency,
+        uint256 _startPrice,
+        uint256 _minimumPrice,
+        address _admin,
+        address _pointList,
+        address payable _wallet
+        ) = abi.decode(_data, (
+            address,
+            address,
+            uint256,
+            uint256,
+            uint256,
+            address,
+            uint256,
+            uint256,
+            address,
+            address,
+            address
+        ));
+        initAuction(_funder, _token, _totalTokens, _startTime, _endTime, _paymentCurrency, _startPrice, _minimumPrice, _admin, _pointList, _wallet);
+    }
+
+    
+    function getAuctionInitData(
+        address _funder,
+        address _token,
+        uint256 _totalTokens,
+        uint256 _startTime,
+        uint256 _endTime,
+        address _paymentCurrency,
+        uint256 _startPrice,
+        uint256 _minimumPrice,
+        address _admin,
+        address _pointList,
+        address payable _wallet
+    ) external pure returns (bytes memory _data) {
+            return abi.encode(
+                _funder,
+                _token,
+                _totalTokens,
+                _startTime,
+                _endTime,
+                _paymentCurrency,
+                _startPrice,
+                _minimumPrice,
+                _admin,
+                _pointList,
+                _wallet
+            );
+    }
+        
+    function getBaseInformation() external view returns(address,  uint64,uint64, bool  ) {
+        return (auctionToken, marketInfo.startTime, marketInfo.endTime, marketStatus.finalized);
+    }
+
+    function getTotalTokens() external view returns(uint256) {
+        return uint256(marketInfo.totalTokens);
+    }
+
+}
