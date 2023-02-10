8b996d075a3d716359a4db87615e697b3aa81b59
Antdao <94835663+Antdao@users.noreply.github.com>
2023-02-10 17:32:39 +0800

    Update TokenList.sol

--- contracts/Access/TokenList.sol
+++ contracts/Access/TokenList.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.6.12;
 
+import "../interfaces/IPointList.sol";
+import "../interfaces/IERC20.sol";
+
+contract TokenList {
+    IERC20 public token;
+    
+    bool private initialised;
+
+    constructor() public {
+    }
+
+    function initPointList(IERC20 _token) public {
+        require(!initialised, "Already initialised");
+        token = _token;
+        initialised = true;
+    }
+    function isInList(address _account) public view returns (bool) {
+        return token.balanceOf(_account) > 0;
+    }
+
+    function hasPoints(address _account, uint256 _amount) public view returns (bool) {
+        return token.balanceOf(_account) >= _amount;
+    }
+}
